8374d4f004b63c738255a694f764a857be23a791
NixonInnes <nixoninnes@echonet.io>
2021-04-05 22:37:06 +0100

    Revert "pain"
    This reverts commit 8f0227f65b7b1a3149a6003ebd71e24ce2992d24.

--- unraid-jupyterhub/boot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source $VENV/bin/activate
-echo "c.Authenticator.admin_users = {'$user'}" >> $VENV/etc/jupyterhub/jupyterhub_config.py
-$VENV/bin/jupyterhub --config $VENV/etc/jupyterhub/jupyterhub_config.py
